fa3b6987f2f90bd232334021c70fa9cfbaee75d9
Sranjan4321 <shashi94ranjan@gmail.com>
2022-08-23 14:49:54 +0530

    initialcommit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </ChakraProvider>
 );
 
 reportWebVitals();
