a55246c670262fbc2a46257ae2cae11fe01e1281
Swapnilk98 <99746007+Swapnilk98@users.noreply.github.com>
2022-08-23 22:34:19 +0530

    update app
    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import LargeWithAppLinksAndSocial from './component/Footer/Footer';
+import WithSubnavigation from './component/Navbar/Navbar';
+
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <WithSubnavigation/>
+   <LargeWithAppLinksAndSocial/>
+  </div>;
 }
 
 export default App;
