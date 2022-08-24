b160513ac9f0736fd32363be192ac84148c2ec8f
Sranjan4321 <shashi94ranjan@gmail.com>
2022-08-24 19:11:09 +0530

    signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import SignupPages from './component/Signup/SignupPages';
+// import SignUp from './component/Signup/SignUp';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <SignUp /> */}
+      <SignupPages />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/component/Signup/SignupPages.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignupPages = () => {
+  return <div></div>;
+};
+
+export default SignupPages;
