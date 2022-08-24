df9e3ed48b8853886449abc2c48157d66ce1f36b
Sranjan4321 <shashi94ranjan@gmail.com>
2022-08-24 21:04:52 +0530

    commit again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,7 @@
 import './App.css';
-import SignupPages from './component/Signup/SignupPages';
-// import SignUp from './component/Signup/SignUp';
 
 function App() {
-  return (
-    <div className="App">
-      {/* <SignUp /> */}
-      <SignupPages />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
