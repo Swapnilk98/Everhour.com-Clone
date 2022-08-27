020261378345a669a8cd9cb9b2090408dc8f32bd
Sranjan4321 <shashi94ranjan@gmail.com>
2022-08-27 20:03:07 +0530

    commit main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
-
-import Setting from './component/Payment/Setting';
-import CombineEmptyandFilled from './component/Products/CombineEmptyandFilled';
-import EmptyProjects from './component/Products/EmptyProjects';
-import Loading from './component/Products/Loading';
-
-import OuterLayout from './component/Products/OuterLayout';
 import Routing from './component/Routing';
 
-
-
 function App() {
-  return <div className="App">
-    {/* <OuterLayout/> */}
-    {/* <Setting/> */}
-    {/* <CombineEmptyandFilled/> */}
-    {/* <Loading/> */}
-    <Routing/>
-  </div>;
-
+  return (
+    <div className="App">
+      <Routing />
+    </div>
+  );
 }
 export default App;
--- frontend/src/component/Routing.jsx
+++ frontend/src/component/Routing.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import DashboardMain from './DashBoardMain/DashboardMain'
-import Confirmation from './Payment/Confirmation'
-import Otp from './Payment/Otp'
-import Setting from './Payment/Setting'
-import CombineEmptyandFilled from './Products/CombineEmptyandFilled'
-import SimpleSidebar from './SideBar/SideBar'
-  
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import DashboardMain from './DashBoardMain/DashboardMain';
+import Navbar from './Navbar/Navbar';
+import Confirmation from './Payment/Confirmation';
+import Otp from './Payment/Otp';
+import Setting from './Payment/Setting';
+import CombineEmptyandFilled from './Products/CombineEmptyandFilled';
+import SimpleSidebar from './SideBar/SideBar';
+
 const Routing = () => {
   return (
-    <Routes>
-      {/* <Route path='/project' element={<CombineEmptyandFilled/>}/> */}
-      <Route path='/otp' element={<Otp/>}/>
-      {/* <Route path='/setting' element={<Setting/>}/> */}
-      <Route path='/confirmation' element={<Confirmation/>}/>
-      <Route path='/dashboard' element={<DashboardMain/>}/>
-    </Routes>
-  )
-}
+    <>
+      <Navbar />
+      <Routes>
+        {/* <Route path='/project' element={<CombineEmptyandFilled/>}/> */}
+        // <Route path="/otp" element={<Otp />} />
+        {/* <Route path='/setting' element={<Setting/>}/> */}
+        <Route path="/confirmation" element={<Confirmation />} />
+        <Route path="/dashboard" element={<DashboardMain />} />
+      </Routes>
+    </>
+  );
+};
 
-export default Routing
+export default Routing;
